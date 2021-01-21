805919f5d7ffd59b1dc6381e917823b12e1d67ae
meganpekarek <mpekarek9@gmail.com>
2021-01-21 16:39:21 -0600

    most of home page completed. still need to tweak styling

--- my-app/src/views/Home.js
+++ my-app/src/views/Home.js
@@ -1,5 +1,8 @@
 import '../App.css';
 import React from 'react'
+import NSACoverPhoto from '../images/NSACoverPhoto.png'
+import SheCanicCoverPhoto from '../images/SheCanicCoverPhoto.png'
+import CGCoverPhoto from '../images/CGCoverPhoto.png'
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faRandom, faUsers } from '@fortawesome/free-solid-svg-icons'
 
@@ -9,29 +12,60 @@ class Home extends React.Component {
   }
 
   render() {
+
+    // const CaseStudies = () => (
+    //     <div>case</div>
+    // );
+
     return (
-      <section className="home__wrapper">
-        <div className="home__headerContent">
-            <div className="home__titleSection">
-                <h1 className="home__header">Megan Pekarek</h1>
-                <h3 className="home__subheader">Front End Developer and UX/UI Designer</h3>
-            </div>
-            <div className="home__infoSection">
-                <div className="home__careerInfo">
-                    <h3>&lt; Developer /&gt;</h3>
-                    <div className="home__headerDesc">
-                    My journey into the world of software started with my degree in Computer Science. I quickly realized that front end development is where it’s at. Bringing beautiful designs to life never gets old.
+        <React.Fragment>
+            <section className="home__wrapper">
+                <div className="home__headerContent">
+                    <div className="home__titleSection">
+                        <h1 className="home__header">Megan Pekarek</h1>
+                        <h3 className="home__subheader">Front End Developer and UX/UI Designer</h3>
+                    </div>
+                    <div className="home__infoSection">
+                        <div className="home__careerInfo">
+                            <h3>&lt; Developer /&gt;</h3>
+                            <div className="home__headerDesc">
+                            My journey into the world of software started with my degree in Computer Science. I quickly realized that front end development is where it’s at. Bringing beautiful designs to life never gets old.
+                            </div>
+                        </div>
+                        <div className="home__careerInfo">
+                            <h3>Designer</h3>
+                            <div className="home__headerDesc">
+                            I love making pretty things, but what I am most passionate about is user research and testing. I want to know what my users want and why they want it. 
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="home__careerInfo">
-                    <h3>Designer</h3>
-                    <div className="home__headerDesc">
-                    I love making pretty things, but what I am most passionate about is user research and testing. I want to know what my users want and why they want it. 
+            </section>
+            <section className="home__uxWorkWrapper">
+                <h3>My UX/UI Work</h3>
+                <div className="home__uxWorkCaseStudies">
+                    <div className="home__caseStudy home__leftAlignedCaseStudy">
+                        <img className="home_caseStudyPhoto" src={CGCoverPhoto}/>
+                        <div className="home__caseStudyDesc">
+                            Common Ground Meditation Center Website Redesign Prototype
+                        </div>
+                    </div>
+                    <div className="home__caseStudy home__rightAlignedCaseStudy">
+                        {/* <img className="home_caseStudyPhoto" src={SheCanicCoverPhoto}/> */}
+                        <div className="home__caseStudyDesc">
+                            SheCanic Vehicle Maintenance Mobile App Prototype
+                        </div>
+                        <img className="home_caseStudyPhoto" src={SheCanicCoverPhoto}/>
+                    </div>
+                    <div className="home__caseStudy home__leftAlignedCaseStudy">
+                        <img className="home_caseStudyPhoto" src={NSACoverPhoto}/>
+                        <div className="home__caseStudyDesc">
+                            <span>NSA Website Redesign Prototype</span>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-      </section>
+            </section>
+        </React.Fragment>
     );
   }
 }
