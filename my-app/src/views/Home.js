0f5ab5ff3a69d5cf0f0c49538611e86570dbc1ed
meganpekarek <mpekarek9@gmail.com>
2021-01-21 15:32:26 -0600

    more spacing fixes

--- my-app/src/views/Home.js
+++ my-app/src/views/Home.js
@@ -17,11 +17,17 @@ class Home extends React.Component {
                 <h3 className="home__subheader">Front End Developer and UX/UI Designer</h3>
             </div>
             <div className="home__infoSection">
-                <div className="home__devInfo">
-                    dev info here
+                <div className="home__careerInfo">
+                    <h3>&lt; Developer /&gt;</h3>
+                    <div className="home__headerDesc">
+                    My journey into the world of software started with my degree in Computer Science. I quickly realized that front end development is where it’s at. Bringing beautiful designs to life never gets old.
+                    </div>
                 </div>
-                <div className="home__designInfo">
-                    design info here
+                <div className="home__careerInfo">
+                    <h3>Designer</h3>
+                    <div className="home__headerDesc">
+                    I love making pretty things, but what I am most passionate about is user research and testing. I want to know what my users want and why they want it. 
+                    </div>
                 </div>
             </div>
         </div>
